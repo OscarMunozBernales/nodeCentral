df6082c552580400a53315d3a0a52ae9c3ec2695
sergioeabarcaf <sergioaf1991@gmail.com>
2017-11-04 15:56:20 -0300

    dev sergioeabarcaf UPDATE app.js: se eliminan funciones de guardado en base de datos local.

--- alerta/app.js
+++ alerta/app.js
@@ -7,36 +7,6 @@ var express = require('express'),
 	Sensor = require("./models/sensor").Sensor,
 	document = require("min-document");
 
-//generar el schema para cargar a la db
-client.on('message', function(topic, message) {
-	splitMessage = message.toString().split("/");
-	//Schema sensores
-	var sensor = new Sensor({
-		paramSensor: String(topic),
-		dato: String(splitMessage[1]),
-		idTotem: String(splitMessage[0]),
-		fechaYHora: Date()
-	});
-
-	//Guardar en la db los datos.
-	sensor.save(function(err) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log("los datos fueron cargados a la db " + sensor.paramSensor);
-		}
-	})
-
-	//Condicional para ejecutar la funcion correspondiente a cada dashboard
-	if(topic=="temperatura"){
-		io.sockets.emit('new temperatura', {
-			value: String(splitMessage[1])
-		});
-		console.log("Emitio el mensaje a new temperatura");
-	}
-
-});
-
 //Puerto donde corre el sistema
 server.listen(8080);
 
@@ -58,6 +28,5 @@ io.sockets.on('connection', function(socket) {
 	socket.on('sendAlert', function(data){
 		var mensaje = "totem1/" + data.value;
 		clientAlert.publish('alerta',mensaje);
-		console.log("se envio el dato por mqtt: " + data.value);
 	})
 });
