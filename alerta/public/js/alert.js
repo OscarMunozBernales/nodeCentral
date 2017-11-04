564b3cbeea17a0c19885e24174cfafa5017254f6
sergioeabarcaf <sergioaf1991@gmail.com>
2017-11-04 15:48:09 -0300

    dev sergioeabarcaf CLEAN alert.js: Se eliminan lineas de log

--- alerta/public/js/alert.js
+++ alerta/public/js/alert.js
@@ -1,9 +1,6 @@
 function updateAlerta(mensaje){
-  console.log("entro a funcion");
   var socket = io.connect();
-
   socket.emit('sendAlert',{
     value:mensaje
   });
-  console.log("se envia mensaje de alerta");
 };
